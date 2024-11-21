44fa1c0393a05c353573ade38fc570a5e36adb40
Nicolas C. <github@liliwol.fr>
2024-11-21 09:30:53 +0100

    Update Error 404 and schema

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,5 +1,11 @@
-/**
- *  Serveur Backend Pokedex
+/*
+
+  ____       _            _             ____             _                  _
+ |  _ \ ___ | | _____  __| | _____  __ | __ )  __ _  ___| | _____ _ __   __| |
+ | |_) / _ \| |/ / _ \/ _` |/ _ \ \/ / |  _ \ / _` |/ __| |/ / _ \ '_ \ / _` |
+ |  __/ (_) |   <  __/ (_| |  __/>  <  | |_) | (_| | (__|   <  __/ | | | (_| |
+ |_|   \___/|_|\_\___|\__,_|\___/_/\_\ |____/ \__,_|\___|_|\_\___|_| |_|\__,_|
+
  */
 
 // Définir l'emplacement des fichiers bases de données
@@ -10,23 +16,27 @@ const PORT = 5001;
 // Définir une écoute sur toutes les interfaces
 const URL = '0.0.0.0';
 
+
+// ************************************************
+// Configuration et Lancement du serveur
 // ************************************************
 
-// Lancer un serveur express sur un port défini
-
+// Importer le module fs pour liore les fichiers
 const fs = require('fs');
-
-// npm install express
+// Importer le module express pour créer un serveur
 const express = require('express');
+// Importer le module cors pour autoriser les requêtes cross-origin
 const cors = require('cors')
+
+// Créer une application express
 const app = express();
 
-// CORS
+// Utiliser le middleware CORS sur l'application (sera utile pour les requêtes cross-origin)
 app.use(cors())
 // Express va servir les fichiers statiques placés dans le dossier FILES
 app.use(express.static('FILES'))
 
-// Lancement du serveur et attendre
+// Lancement du serveur et attendre les connexions
 app.listen(
     PORT,
     // Ecoute sur toutes les interfaces
@@ -41,6 +51,11 @@ app.listen(
 
 
 // *********************************************
+// Définition des routes
+// *********************************************
+
+
+// *********************************************
 // Route POKEMON ALL
 // Path: /
 // Method: GET
@@ -48,9 +63,55 @@ app.listen(
 app.get(
     '/',
     findAllPokemon
-)
+);
 
+// *********************************************
+// Route POKEMON RANDOM
+// Path: /hasard
+// Method: GET
+// *********************************************
+app.get('/hasard', findByIdRandomly);
+
+// *********************************************
+// Route POKEMON BY ID
+// Path: /pokemon/:id (id doit être un entier)
+// Method: GET
+// *********************************************
+app.get('/pokemon/:id(\\d+)', findById);
+
+// *********************************************
+// Route POKEMON BY NAME
+// Le nom doit être le nom Français du Pokemon
+// Path: /pokemon/name/:name (name doit être une string)
+// Method: GET
+// *********************************************
+app.get('/pokemon/name/:name', findByName);
+
+// *********************************************
+// Route POKEMON BY TYPE
+// Path: /pokemon/type/:type (type doit être une string)
+// Method: GET
+// *********************************************
+app.get('/pokemon/type/:type', findByType);
+
+// *********************************************
+// Route POKEMON BY HP
+// Path: /pokemon/hp/:hp (type doit être un entier)
+// Method: GET
+// *********************************************
+app.get('/pokemon/hp/:hp', findByHP);
+
+
+
+
+
+// *********************************************
+// Définition des fonctions de gestion des routes
+// *********************************************
+
+// *********************************************
 // Fonction qui est appelée par la route / en GET
+// *********************************************
 function findAllPokemon(request, response)
 {
     // Appel de la fonction de debug des routes
@@ -66,16 +127,9 @@ function findAllPokemon(request, response)
     response.send(pokedex);
 }
 
-
-
-
 // *********************************************
-// Route POKEMON RANDOM
-// Path: /hasard
-// Method: GET
+// Fonction qui est appelée par la route /hasard en GET
 // *********************************************
-app.get('/hasard', findByIdRandomly);
-
 function findByIdRandomly(request, response)
 {
     // Appel de la fonction de debug des routes
@@ -90,7 +144,7 @@ function findByIdRandomly(request, response)
     // 3. Au hasard
     let nb_pokemon = pokedex.length;
     console.log("Nb total de pokemon " + nb_pokemon);
-    
+
     let random = Math.floor(Math.random() * nb_pokemon) + 1;
     console.log("Pokemon au hasard " + random);
 
@@ -100,15 +154,9 @@ function findByIdRandomly(request, response)
     response.send(reply);
 }
 
-
-
 // *********************************************
-// Route POKEMON BY ID
-// Path: /pokemon/:id (id doit être un entier)
-// Method: GET
+// Fonction qui est appelée par la route /pokemon/:id en GET
 // *********************************************
-app.get('/pokemon/:id(\\d+)', findById);
-
 function findById(request, response)
 {
     // Appel de la fonction de debug des routes
@@ -130,24 +178,20 @@ function findById(request, response)
         reply = pokedex[id -1];
     }
     else {
-        reply = {
-            status:"Not Found"
-        }
+        reply = false;
     }
 
-    response.send(reply);
+    // Envoi des données
+    if (reply) {
+        response.send(reply);
+    } else {
+        response.status(404).send('Erreur, pokemon pas trouvé!');
+    }
 }
 
-
-
 // *********************************************
-// Route POKEMON BY NAME
-// Le nom doit être le nom Français du Pokemon
-// Path: /pokemon/name/:name (name doit être une string)
-// Method: GET
+// Fonction qui est appelée par la route /pokemon/name en GET
 // *********************************************
-app.get('/pokemon/name/:name', findByName);
-
 function findByName(request, response)
 {
     // Appel de la fonction de debug des routes
@@ -169,16 +213,17 @@ function findByName(request, response)
         (pokemon) => pokemon.name.french.toUpperCase() === name
     );
 
-    response.send(reply);
+    // Envoi des données
+    if (reply.length > 0) {
+        response.send(reply);
+    } else {
+        response.status(404).send('Erreur, pokemon pas trouvé!');
+    }
 }
 
 // *********************************************
-// Route POKEMON BY TYPE
-// Path: /pokemon/type/:type (type doit être une string)
-// Method: GET
+// Fonction qui est appelée par la route /type en GET
 // *********************************************
-app.get('/pokemon/type/:type', findByType);
-
 function findByType(request, response)
 {
     // Appel de la fonction de debug des routes
@@ -205,9 +250,51 @@ function findByType(request, response)
             )
     );
 
-    response.send(reply);
+    // Envoi des données
+    if (reply.length > 0) {
+        response.send(reply);
+    } else {
+        response.status(404).send('Erreur, pokemon pas trouvé!');
+    }
 }
 
+// *********************************************
+// Fonction qui est appelée par la route /type en GET
+// *********************************************
+function findByHP(request, response)
+{
+    // Appel de la fonction de debug des routes
+    debugRoute(request);
+
+    // 1. Lecture du fichier
+    let data = fs.readFileSync(POKEDEX_SRC);
+
+    // 2. Analyse du JSON
+    let pokedex = JSON.parse(data);
+
+    // Récupération du paramètre
+    let hp = request.params.hp;
+
+    console.log(`Recherche du nombre de HP: ${hp}`);
+
+    const reply = pokedex.filter(
+        (pokemon) =>
+            pokemon.base.HP === hp
+    );
+
+    // Envoi des données
+    if (reply.length > 0) {
+        response.send(reply);
+    } else {
+        response.status(404).send('Erreur, pokemon pas trouvé!');
+    }
+}
+
+
+
+// *********************************************
+// Fonction de debug des routes
+// *********************************************
 function debugRoute(request){
     console.log("---------------------------");
     console.log("Route appelée: " + request.url);
